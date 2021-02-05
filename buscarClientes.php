5d10623d0b81c54bc70933d8f8abe638997b52ec
JuarezVega <alej.juarez.vega@gmail.com>
2021-02-05 21:35:08 +0100

    version 3

--- buscarClientes.php
+++ buscarClientes.php
@@ -4,43 +4,21 @@ $username = "php";
 $password = "1234ajv";
 $dbname = "prueba";
 
+require "claseCliente.php";
+
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 // creacion de la busqueda
 $filtro = $_POST["filtro"];
 $busqueda = $_POST["buscar"];
 
-$sql = "SELECT nombre, apellidos, dni, email, fecha_nac FROM clientes where ".$filtro." like '%".$busqueda."%';";
+$clienteIreal = new cliente('prueba','prueba','prueba','prueba','prueba@prueba.com');
+$clienteIreal->buscar($filtro,$busqueda,$conn);
 
-$result = mysqli_query($conn, $sql);
- // busqueda en si
-echo "<table border='1'>
-        <tr>
-            <th>Nombre</th>
-            <th>Apellidos</th>
-            <th>DNI</th>
-            <th>Email</th>
-            <th>Fecha de nacimiento</th>
-        </tr>";
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($registro = mysqli_fetch_assoc($result)) {
-        echo "<tr>"
-        . "<td>" . $registro["nombre"]. "</td>"
-        . "<td>". $registro["apellidos"]."</td>"
-        ."<td>".$registro["dni"]. "</td>"
-        ."<td>".$registro["email"]. "</td>"
-        ."<td>".$registro["fecha_nac"]. "</td>"
-. "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-echo "</table>";
-mysqli_close($conn);
+$conn->close($conn);
 ?>
