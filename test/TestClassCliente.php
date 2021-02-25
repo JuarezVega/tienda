0b0c747570495e855fb747eaeaac98dfdaf11406
JuarezVega <alej.juarez.vega@gmail.com>
2021-02-25 18:13:56 +0000

    version 3.1

---
+++ test/TestClassCliente.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SalarioTest
+ *
+ * @author -andrés
+ */
+
+require 'vendor/autoload.php';
+require 'claseCliente.php';
+
+class productoTest extends \PHPUnit\Framework\TestCase
+{
+
+
+    public function testDarAlta()
+    {
+
+        $servername = "localhost";
+        $username = "php";
+        $password = "1234";
+        $dbname = "prueba";
+
+        // Establecer conexión con la base de datos
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Verificar la conexión
+        if ($conn->connect_error) {
+            die("Error de conexión: " . $conn->connect_error);
+        }
+
+
+        //Primera tanda
+        //Primero calculo cuantas lineas hay en la tabla
+        $sqlPrueba = "select * from productos;";
+        $resultado = $conn->query($sqlPrueba);
+
+        // Consulta para realizar la busqueda en la base de datos
+        $clientesAntes = $resultado->num_rows;
+
+
+        $clienteNuevo = new producto(69, "prueba", 69, 69);
+
+        $clienteNuevo->darAlta($conn);
+
+        $resultado = $conn->query($sqlPrueba);
+
+        // Consulta para realizar la busqueda en la base de datos
+        $clientesDespues = $resultado->num_rows;
+
+
+        $this->assertEquals($clientesAntes + 1, $clientesDespues, "El producto se da de alta correctamente");
+
+    }
